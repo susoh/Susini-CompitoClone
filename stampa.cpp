3860742ecc9d1ca40a9340da058a17a675f56029
susoh <susini.davide@itismeucci.com>
2024-05-14 10:31:45 +0200

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -8,12 +8,13 @@ Davide Susini 14/05/24
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0 ,n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
    cout<<"Bada come la fuma"<<endl; 
+   i++;
   }
   system("pause");
 }
